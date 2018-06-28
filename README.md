8cf7328ec7094c6ffc5ae737742db565e4e3d370
topqiang <topqiang@kanziqiang.top>
2018-06-28 15:30:14 +0800

    初始化带布局的后台

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-# carmanage
+# GMA
+admin template based on [vuejs2](http://vuejs.org/) and [element](http://element.eleme.io/#/) and [vuex](https://github.com/vuejs/vuex).
 
-> 众享车驿站管理系统
+### Usage
 
-## Build Setup
+This is a project template for [vue-cli](https://github.com/vuejs/vue-cli).
 
 ``` bash
 # install dependencies
 npm install
 
-# serve with hot reload at localhost:8080
+# serve with hot reload at localhost:8081
 npm run dev
 
 # build for production with minification
 npm run build
 
-# build for production and view the bundle analyzer report
-npm run build --report
 ```
+### Snapshot
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+ ![image](https://github.com/chjw8016/gma/raw/master/static/20170321160116.png)
+ ![image](https://github.com/chjw8016/gma/raw/master/static/20170321161916.png)
+
+### License
+[MIT](http://opensource.org/licenses/MIT)
--- src/main.js
+++ src/main.js
@@ -1,29 +1,53 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import axios from 'axios'
-import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+//import 'element-ui/lib/theme-default/index.css'
+import './assets/theme/theme-gray/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import routes from './routes'
+import Mock from './mock'
+Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.config.productionTip = false
-// 设置默认的参数请求方式
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-//将对象转成序列
-axios.defaults.transformRequest= [function (data) {
-	let ret = ''
-	for (let it in data) {
-		ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-	}
-	return ret
-}];
-Vue.prototype.$http = axios
+window.$ = window.jQuery = require('jquery');
+
 Vue.use(ElementUI)
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+    routes
 })
+
+router.beforeEach((to, from, next) => {
+    //NProgress.start();
+    if (to.path == '/login') {
+        sessionStorage.removeItem('user');
+    }
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user && to.path != '/login') {
+        next({ path: '/login' })
+    } else {
+        next()
+    }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+    //el: '#app',
+    //template: '<App/>',
+    router,
+    store,
+    //components: { App }
+    render: h => h(App)
+}).$mount('#app')
